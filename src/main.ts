c41f7b71738f6ff52610c6bd368042a91f9c9d49
thewrath <thomaslegoff56@laposte.net>
2024-07-10 07:56:41 +0200

    feat: add debugging

--- src/main.ts
+++ src/main.ts
@@ -7,11 +7,15 @@ import tiles from './tiles.png';
 import * as LittleJS from 'littlejsengine';
 import { World } from 'miniplex';
 
-const size = LittleJS.vec2(1);
-const minSpeed = 0.001;
-const maxSpeed = 0.003;
+// LittleJS settings
+LittleJS.setCameraScale(32);
+// LittleJS.setCanvasMaxSize(LittleJS.vec2(1280, 720));
 
-const numberOfEntities = 10;
+const size = LittleJS.vec2(1);
+const minSpeed = 0.01;
+const maxSpeed = 0.03;
+
+const numberOfEntities = 100;
 
 const world: World<Entity> = new World<Entity>();
 
@@ -33,6 +37,7 @@ interface EntityType {
   enemyTypeName: EntityTypeName
 }
 
+let worldSize: LittleJS.Vector2;
 let entityTypes: EntityType[];
 let randomGenerator: LittleJS.RandomGenerator;
 
@@ -47,6 +52,8 @@ let queries = {
 }
 
 function initGame() {
+  // worldSize = LittleJS.screenToWorld(LittleJS.canvasMaxSize);
+  worldSize = LittleJS.vec2(-15, 15);
 
   randomGenerator = new LittleJS.RandomGenerator(Date.now());
 
@@ -59,11 +66,65 @@ function initGame() {
   for (let i = 0; i < numberOfEntities; i++) {
     world.add(_initEntity(entityTypes[randomGenerator.int(3)]));
   }
+
+  // _initPostProcess();
 }
 
-function _initEntity(type: EntityType): Entity {
+function _initPostProcess() {
+    const televisionShader = `
+    // Simple TV Shader Code
+    float hash(vec2 p)
+    {
+        p=fract(p*.3197);
+        return fract(1.+sin(51.*p.x+73.*p.y)*13753.3);
+    }
+    float noise(vec2 p)
+    {
+        vec2 i=floor(p),f=fract(p),u=f*f*(3.-2.*f);
+        return mix(mix(hash(i),hash(i+vec2(1,0)),u.x),mix(hash(i+vec2(0,1)),hash(i+1.),u.x),u.y);
+    }
+    void mainImage(out vec4 c, vec2 p)
+    {
+        // put uv in texture pixel space
+        p /= iResolution.xy;
+
+        // apply fuzz as horizontal offset
+        const float fuzz = .0005;
+        const float fuzzScale = 800.;
+        const float fuzzSpeed = 9.;
+        p.x += fuzz*(noise(vec2(p.y*fuzzScale, iTime*fuzzSpeed))*2.-1.);
+
+        // init output color
+        c = texture(iChannel0, p);
+
+        // chromatic aberration
+        const float chromatic = .002;
+        c.r = texture(iChannel0, p - vec2(chromatic,0)).r;
+        c.b = texture(iChannel0, p + vec2(chromatic,0)).b;
+
+        // tv static noise
+        const float staticNoise = .1;
+        c += staticNoise * hash(p + mod(iTime, 1e3));
+
+        // scan lines
+        const float scanlineScale = 1e3;
+        const float scanlineAlpha = .1;
+        c *= 1. + scanlineAlpha*sin(p.y*scanlineScale);
+
+        // black vignette around edges
+        const float vignette = 2.;
+        const float vignettePow = 6.;
+        float dx = 2.*p.x-1., dy = 2.*p.y-1.;
+        c *= 1.-pow((dx*dx + dy*dy)/vignette, vignettePow);
+    }`;
+
+    const includeOverlay = true;
+    LittleJS.glInitPostProcess(televisionShader, includeOverlay);
+}
+
+function _initEntity(type: EntityType): Entity {  
   return {
-    position: LittleJS.vec2(randomGenerator.int(-15, 15), randomGenerator.int(-15, 15)),
+    position: LittleJS.vec2(randomGenerator.int(-worldSize.x, worldSize.x), randomGenerator.int(-worldSize.y, worldSize.y)),
     velocity: LittleJS.vec2(randomGenerator.float(minSpeed, maxSpeed), randomGenerator.float(minSpeed, maxSpeed)),
     tileInfo: type.tileInfo,
     [type.typeName]: type
@@ -106,7 +167,7 @@ function _solveConflicts() {
 
     for (const { position: aPos } of allies) {
       for (const enemy of enemies) {
-        if (_checkCollision(aPos, enemy.position, size)) {
+        if (LittleJS.isOverlapping(aPos, size, enemy.position, size)) {
           world.removeComponent(enemy, type.enemyTypeName);
           world.addComponent(enemy, type.typeName, type);
           enemy.tileInfo = type.tileInfo;
@@ -132,11 +193,26 @@ function _goToTarget() {
 
 function renderGame() {
   _renderEntities();
+  _renderDebug();
 }
 
 function _renderEntities() {
   for (const { position, tileInfo } of queries.drawing) {
     LittleJS.drawTile(position, size, tileInfo);
+  }
+}
+
+function _renderDebug() {
+  if (!LittleJS.debug) return;
+
+  for (const { position } of queries.drawing) {
+    LittleJS.debugRect(position, size);
+  }
+
+  for (const { position, target } of queries.attacking) {
+    if (LittleJS.isOverlapping(position, size, LittleJS.mousePos, LittleJS.vec2(1, 1))) {
+      LittleJS.drawLine(position, target.position);
+    }
   }
 }
 
@@ -165,5 +241,5 @@ function _moveTowards(targetPos: LittleJS.Vector2, currentPos: LittleJS.Vector2,
 
   if (delta.length() === 0) return currentPos; // Already at the target
 
-  return currentPos.add(delta.multiply(velocity));
+  return currentPos.add(delta.normalize().multiply(velocity));
 }
